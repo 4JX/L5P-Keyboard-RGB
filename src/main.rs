e0c5b7208f83e253624ebd4a939d26dd3568ae55
4JX <79868816+4JX@users.noreply.github.com>
2021-09-07 00:57:14 +0200

    Add logic for OS specific compilation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// #![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 use fltk::app;
 use parking_lot::Mutex;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -8,6 +8,7 @@ use tray_item::{IconSource, TrayItem};
 mod gui;
 mod keyboard_utils;
 
+#[cfg(target_os = "windows")]
 static SHOW_GUI: AtomicBool = AtomicBool::new(true);
 
 fn main() {
@@ -17,27 +18,37 @@ fn main() {
 		Err(err) => panic!("{}", err),
 	};
 
-	let mut tray = TrayItem::new("Backlight RGB", IconSource::Resource("trayIcon")).unwrap();
+	#[cfg(target_os = "windows")]
+	{
+		//Create tray icon
+		let mut tray = TrayItem::new("Backlight RGB", IconSource::Resource("trayIcon")).unwrap();
 
-	tray.add_menu_item("Show", move || {
-		SHOW_GUI.store(true, Ordering::Relaxed);
-	})
-	.unwrap();
+		tray.add_menu_item("Show", move || {
+			SHOW_GUI.store(true, Ordering::Relaxed);
+		})
+		.unwrap();
 
-	tray.add_menu_item("Quit", || {
-		println!("Quit");
-		std::process::exit(0);
-	})
-	.unwrap();
-	loop {
-		if SHOW_GUI.load(Ordering::Relaxed) {
-			println!("Show");
-			gui::start_ui(keyboard.clone());
-			app.run().unwrap();
-			SHOW_GUI.store(false, Ordering::Relaxed);
-		} else {
-			println!("Hide");
-			app::sleep(0.05);
+		tray.add_menu_item("Quit", || {
+			println!("Quit");
+			std::process::exit(0);
+		})
+		.unwrap();
+
+		//Tray loop
+		loop {
+			if SHOW_GUI.load(Ordering::Relaxed) {
+				gui::start_ui(keyboard.clone());
+				app.run().unwrap();
+				SHOW_GUI.store(false, Ordering::Relaxed);
+			} else {
+				app::sleep(0.05);
+			}
 		}
+	}
+
+	#[cfg(not(target_os = "windows"))]
+	{
+		gui::start_ui(keyboard.clone());
+		app.run().unwrap();
 	}
 }
