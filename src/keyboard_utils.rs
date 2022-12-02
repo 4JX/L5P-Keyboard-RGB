c5958e0cf145af6f62aa77f0c7ec2287be1b4aea
Pavel Sevcik <pavel@sevcik.tk>
2022-12-02 18:50:16 +0700

    Add DEVICE_INFO_IDEAPAD_2022

--- src/keyboard_utils.rs
+++ src/keyboard_utils.rs
@@ -18,6 +18,8 @@ const DEVICE_INFO_2021: (u16, u16, u16, u16) = (0x048d, 0xc965, 0, 0);
 const DEVICE_INFO_2020: (u16, u16, u16, u16) = (0x048d, 0xc955, 0, 0);
 #[cfg(target_os = "linux")]
 const DEVICE_INFO_IDEAPAD_2021: (u16, u16, u16, u16) = (0x048d, 0xc963, 0, 0);
+#[cfg(target_os = "linux")]
+const DEVICE_INFO_IDEAPAD_2022: (u16, u16, u16, u16) = (0x048d, 0xc973, 0, 0);
 #[cfg(target_os = "windows")]
 const DEVICE_INFO_2022: (u16, u16, u16, u16) = (0x048d, 0xc975, 0xff89, 0x00cc);
 #[cfg(target_os = "windows")]
@@ -26,6 +28,8 @@ const DEVICE_INFO_2021: (u16, u16, u16, u16) = (0x048d, 0xc965, 0xff89, 0x00cc);
 const DEVICE_INFO_2020: (u16, u16, u16, u16) = (0x048d, 0xc955, 0xff89, 0x00cc);
 #[cfg(target_os = "windows")]
 const DEVICE_INFO_IDEAPAD_2021: (u16, u16, u16, u16) = (0x048d, 0xc963, 0xff89, 0x00cc);
+#[cfg(target_os = "windows")]
+const DEVICE_INFO_IDEAPAD_2022: (u16, u16, u16, u16) = (0x048d, 0xc973, 0xff89, 0x00cc);
 
 const SPEED_RANGE: std::ops::Range<u8> = 1..5;
 const BRIGHTNESS_RANGE: std::ops::Range<u8> = 1..3;
@@ -207,7 +211,7 @@ pub fn get_keyboard(stop_signal: Arc<AtomicBool>) -> Result<Keyboard, error::Err
 		.device_list()
 		.find(|d| {
 			let info_tuple = (d.vendor_id(), d.product_id(), d.usage_page(), d.usage());
-			info_tuple == DEVICE_INFO_2021 || info_tuple == DEVICE_INFO_2020 || info_tuple == DEVICE_INFO_IDEAPAD_2021 || info_tuple == DEVICE_INFO_2022
+			info_tuple == DEVICE_INFO_2021 || info_tuple == DEVICE_INFO_2020 || info_tuple == DEVICE_INFO_IDEAPAD_2021 || info_tuple == DEVICE_INFO_2022 || info_tuple == DEVICE_INFO_IDEAPAD_2022
 		})
 		.ok_or(error::Error::DeviceNotFound)?;
 
