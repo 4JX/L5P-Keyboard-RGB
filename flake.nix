dfc37c0dd8e272d7235872022b59148abc682bb7
4JX <79868816+4JX@users.noreply.github.com>
2022-12-30 17:46:09 +0100

    Fix execution issues on nix build

--- flake.nix
+++ flake.nix
@@ -39,36 +39,52 @@
         nixLib = nixpkgs.lib;
         craneLib = (crane.mkLib pkgs).overrideToolchain rust;
 
-        # TODO: Make this more specific
-        protoFilter = path: _type: builtins.match ".*proto$" path != null;
-        vpxHeaderFileFilter = path: _type: builtins.match ".*h$" path != null;
-        resFileFilter = path: _type: builtins.match ".*/res/.*" path != null;
+        # Libraries needed both at compile and runtime
+        sharedDeps = with pkgs; [
+          dbus
+          xorg.libX11
+          libusb
+          pango
+          xorg.libXinerama
+          xorg.libXcursor
+          xorg.libXfixes
+        ];
+
+        # Libraries needed only at runtime
+        runtimeDeps = with pkgs; [
+          fontconfig
+          gobject-introspection
+          glib
+          cairo
+          xorg.libxcb
+        ] ++ sharedDeps;
+
+        # Allow a few more files to be included in the build workspace
+        workspaceSrc = ./.;
+        workspaceSrcString = builtins.toString workspaceSrc;
+
+        protoFilter = path: _type: builtins.match "${workspaceSrcString}/libs/hbb_common/protos/.*.proto$" path != null;
+        vpxHeaderFileFilter = path: _type: builtins.match "${workspaceSrcString}/libs/scrap/vpx_ffi.h$" path != null;
+        resFileFilter = path: _type: builtins.match "${workspaceSrcString}/res/.*" path != null;
         workspaceFilter = path: type:
           (protoFilter path type) || (vpxHeaderFileFilter path type) || (resFileFilter path type) || (craneLib.filterCargoSources path type);
 
-        my-crate = craneLib.buildPackage
+        # The main application derivation
+        legion-kb-rgb = craneLib.buildPackage
           rec {
             src = nixLib.cleanSourceWith
               {
-                src = ./.;
+                src = workspaceSrc;
                 filter = workspaceFilter;
               };
 
             buildInputs = with pkgs;
               [
-                dbus
-                xorg.libX11
-                libusb
-                pango
                 libvpx
                 libyuv
-                xorg.libXinerama
-                xorg.libXcursor
-                xorg.libXfixes
-              ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-
-                # pkgs.libiconv
-              ];
+              ]
+              ++ sharedDeps
+              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
 
             nativeBuildInputs = with pkgs;
               [
@@ -78,10 +94,7 @@
                 clang
                 gcc
                 ninja
-              ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-
-                # pkgs.libiconv
-              ];
+              ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
 
             # Manually simulate a vcpkg installation so that it can link the libaries
             # properly. Borrowed from: https://github.com/NixOS/nixpkgs/blob/69a35ff92dc404bf04083be2fad4f3643b2152c9/pkgs/applications/networking/remote/rustdesk/default.nix#L51
@@ -119,22 +132,36 @@
             RUSTFLAGS = "-Clink-arg=-fuse-ld=${MOLD_PATH} -Clinker=clang";
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           };
+
+        # Wrap the program for ease of use
+        wrappedProgram = pkgs.symlinkJoin rec {
+          name = "legion-kb-rgb";
+          paths = [ legion-kb-rgb ];
+
+          buildInputs = [ pkgs.makeWrapper ];
+
+          postBuild = ''
+            wrapProgram $out/bin/${name} \
+              --prefix LD_LIBRARY_PATH : ${nixLib.makeLibraryPath runtimeDeps}
+          '';
+        };
       in
       {
         checks = {
-          inherit my-crate;
+          inherit legion-kb-rgb;
         };
 
-        packages.default = my-crate;
+        packages.default = legion-kb-rgb;
+        packages.wrapped = wrappedProgram;
 
         apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
+          drv = wrappedProgram;
         };
 
         devShells.default = pkgs.mkShell {
           inputsFrom = builtins.attrValues self.checks;
 
-          inherit (my-crate) buildInputs;
+          inherit (legion-kb-rgb) buildInputs;
 
           # Extra inputs can be added here
           nativeBuildInputs = [
