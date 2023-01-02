6a63ea4a34d925f47c035e8827d26fc2d0681131
4JX <79868816+4JX@users.noreply.github.com>
2023-01-02 23:11:32 +0100

    Update hidapi

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -10,22 +10,12 @@ use std::{
 
 pub mod error;
 
-#[cfg(target_os = "linux")]
-const DEVICE_INFO_2022: (u16, u16, u16, u16) = (0x048d, 0xc975, 0, 0);
-#[cfg(target_os = "linux")]
-const DEVICE_INFO_2021: (u16, u16, u16, u16) = (0x048d, 0xc965, 0, 0);
-#[cfg(target_os = "linux")]
-const DEVICE_INFO_2020: (u16, u16, u16, u16) = (0x048d, 0xc955, 0, 0);
-#[cfg(target_os = "linux")]
-const DEVICE_INFO_IDEAPAD_2021: (u16, u16, u16, u16) = (0x048d, 0xc963, 0, 0);
-#[cfg(target_os = "windows")]
-const DEVICE_INFO_2022: (u16, u16, u16, u16) = (0x048d, 0xc975, 0xff89, 0x00cc);
-#[cfg(target_os = "windows")]
-const DEVICE_INFO_2021: (u16, u16, u16, u16) = (0x048d, 0xc965, 0xff89, 0x00cc);
-#[cfg(target_os = "windows")]
-const DEVICE_INFO_2020: (u16, u16, u16, u16) = (0x048d, 0xc955, 0xff89, 0x00cc);
-#[cfg(target_os = "windows")]
-const DEVICE_INFO_IDEAPAD_2021: (u16, u16, u16, u16) = (0x048d, 0xc963, 0xff89, 0x00cc);
+const KNOWN_DEVICE_INFOS: [(u16, u16, u16, u16); 4] = [
+	(0x048d, 0xc975, 0xff89, 0x00cc), // 2022
+	(0x048d, 0xc965, 0xff89, 0x00cc), // 2021
+	(0x048d, 0xc963, 0xff89, 0x00cc), // 2021 Ideapad
+	(0x048d, 0xc955, 0xff89, 0x00cc), // 2020
+];
 
 const SPEED_RANGE: std::ops::Range<u8> = 1..5;
 const BRIGHTNESS_RANGE: std::ops::Range<u8> = 1..3;
@@ -206,8 +196,17 @@ pub fn get_keyboard(stop_signal: Arc<AtomicBool>) -> Result<Keyboard, error::Err
 	let info = api
 		.device_list()
 		.find(|d| {
-			let info_tuple = (d.vendor_id(), d.product_id(), d.usage_page(), d.usage());
-			info_tuple == DEVICE_INFO_2021 || info_tuple == DEVICE_INFO_2020 || info_tuple == DEVICE_INFO_IDEAPAD_2021 || info_tuple == DEVICE_INFO_2022
+			#[cfg(target_os = "windows")]
+			{
+				let info_tuple = (d.vendor_id(), d.product_id(), d.usage_page(), d.usage());
+				KNOWN_DEVICE_INFOS.iter().any(|known| known == info_tuple);
+			}
+
+			#[cfg(target_os = "linux")]
+			{
+				let info_tuple = (d.vendor_id(), d.product_id());
+				KNOWN_DEVICE_INFOS.iter().map(|known| (known.0, known.1)).any(|known| known == info_tuple)
+			}
 		})
 		.ok_or(error::Error::DeviceNotFound)?;
 
