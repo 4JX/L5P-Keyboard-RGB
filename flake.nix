e79c017d418212e3bbc2fdb8d20be3eac118b628
4JX <79868816+4JX@users.noreply.github.com>
2023-12-30 22:42:22 +0100

    waw

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
         };
 
         nixLib = nixpkgs.lib;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
 
         # Libraries needed both at compile and runtime
         sharedDeps = with pkgs; [
@@ -60,141 +59,25 @@
           freetype
           xorg.libXrandr
           libGL
+
+          mesa
+          xorg.libX11
+          xorg.libXxf86vm
+          xorg.libXi
+
+          wayland
+          libxkbcommon
         ] ++ sharedDeps;
-
-
-
-        # Manually simulate a vcpkg installation so that it can link the libaries
-        # properly. Borrowed and adapted from: https://github.com/NixOS/nixpkgs/blob/69a35ff92dc404bf04083be2fad4f3643b2152c9/pkgs/applications/networking/remote/rustdesk/default.nix#L51
-        vcpkg = pkgs.stdenv.mkDerivation {
-          pname = "vcpkg";
-          version = "1.0.0";
-
-          unpackPhase =
-            let
-              vcpkg_target = "x64-linux";
-
-              updates_vcpkg_file = pkgs.writeText "update_vcpkg_legion-kb-rgb"
-                ''
-                  Package : libyuv
-                  Architecture : ${vcpkg_target}
-                  Version : 1.0
-                  Status : is installed
-                  Package : libvpx
-                  Architecture : ${vcpkg_target}
-                  Version : 1.0
-                  Status : is installed
-                '';
-            in
-            ''
-              mkdir -p vcpkg/.vcpkg-root
-              mkdir -p vcpkg/installed/${vcpkg_target}/lib
-              mkdir -p vcpkg/installed/vcpkg/updates
-              ln -s ${updates_vcpkg_file} vcpkg/installed/vcpkg/status
-              mkdir -p vcpkg/installed/vcpkg/info
-              touch vcpkg/installed/vcpkg/info/libyuv_1.0_${vcpkg_target}.list
-              touch vcpkg/installed/vcpkg/info/libvpx_1.0_${vcpkg_target}.list
-              ln -s ${pkgs.libvpx.out}/lib/* vcpkg/installed/${vcpkg_target}/lib/
-              ln -s ${pkgs.libyuv.out}/lib/* vcpkg/installed/${vcpkg_target}/lib/
-            '';
-
-          installPhase = ''
-            cp -r vcpkg $out
-          '';
-        };
-
-        envVars = rec {
-          RUST_BACKTRACE = 1;
-          MOLD_PATH = "${pkgs.mold.out}/bin/mold";
-          RUSTFLAGS = "-Clink-arg=-fuse-ld=${MOLD_PATH} -Clinker=clang";
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          VCPKG_ROOT = "${vcpkg.out}";
-        };
-
-        # Allow a few more files to be included in the build workspace
-        workspaceSrc = ./.;
-        workspaceSrcString = builtins.toString workspaceSrc;
-
-        resFileFilter = path: _type: builtins.match "${workspaceSrcString}/app/res/.*" path != null;
-        workspaceFilter = path: type:
-          (resFileFilter path type) || (craneLib.filterCargoSources path type);
-
-
-        src = nixLib.cleanSourceWith
-          {
-            src = workspaceSrc;
-            filter = workspaceFilter;
-          };
-
-        buildInputs = with pkgs;
-          [
-            libvpx
-            libyuv
-          ]
-          ++ sharedDeps
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
-
-        nativeBuildInputs = with pkgs;
-          [
-            pkg-config
-            cmake
-            clang
-          ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
-
-        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
-
-        cargoArtifacts = craneLib.buildDepsOnly ({
-          inherit (craneLib.crateNameFromCargoToml { cargoToml = ./app/Cargo.toml; }) pname version;
-
-          inherit src buildInputs nativeBuildInputs stdenv;
-
-          # This magic thing here is needed otherwise the build fails
-          # https://github.com/ipetkov/crane/issues/411#issuecomment-1743441117
-          # Might revisit later to see if anything else pops up
-          installCargoArtifactsMode = "use-zstd";
-        } // envVars);
-
-        # The main application derivation
-        legion-kb-rgb = craneLib.buildPackage
-          ({
-            inherit (craneLib.crateNameFromCargoToml { cargoToml = ./app/Cargo.toml; }) pname version;
-
-            inherit src cargoArtifacts buildInputs nativeBuildInputs stdenv;
-
-            doCheck = false;
-
-            # cargoBuildCommand = "cargo build";
-          } // envVars);
-
-        # Wrap the program for ease of use
-        wrappedProgram = pkgs.symlinkJoin rec {
-          name = "legion-kb-rgb";
-          paths = [ legion-kb-rgb ];
-
-          buildInputs = [ pkgs.makeWrapper ];
-
-          postBuild = ''
-            wrapProgram $out/bin/${name} \
-              --prefix LD_LIBRARY_PATH : ${nixLib.makeLibraryPath runtimeDeps}
-          '';
-        };
       in
       {
         checks = {
           # inherit legion-kb-rgb;
         };
 
-        packages.default = legion-kb-rgb;
-        packages.wrapped = wrappedProgram;
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH = nixLib.makeLibraryPath runtimeDeps;
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = wrappedProgram;
-        };
-
-        devShells.default = legion-kb-rgb;
-        devShells.rust = pkgs.mkShell {
-          buildInputs = [ rust ];
+          packages = [ rust ];
         };
       });
 }
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Confirm exit",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    show_confirmation_dialog: bool,
+    allowed_to_close: bool,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Try to close the window");
+        });
+
+        if ctx.input(|i| i.viewport().close_requested()) {
+            if self.allowed_to_close {
+                // do nothing - we will close
+            } else {
+                ctx.send_viewport_cmd(egui::ViewportCommand::CancelClose);
+                self.show_confirmation_dialog = true;
+            }
+        }
+
+        if self.show_confirmation_dialog {
+            egui::Window::new("Do you want to quit?")
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        if ui.button("No").clicked() {
+                            self.show_confirmation_dialog = false;
+                            self.allowed_to_close = false;
+                        }
+
+                        if ui.button("Yes").clicked() {
+                            self.show_confirmation_dialog = false;
+                            self.allowed_to_close = true;
+                            ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                });
+        }
+    }
+}
