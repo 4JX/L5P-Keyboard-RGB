18d40c25f6171855cc5646a1ead234ead0b318d9
Drakanio <83156875+Drakanio@users.noreply.github.com>
2024-02-09 18:45:24 +0100

    Update lib.rs

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -11,7 +11,8 @@ use std::{
 
 pub mod error;
 
-const KNOWN_DEVICE_INFOS: [(u16, u16, u16, u16); 8] = [
+const KNOWN_DEVICE_INFOS: [(u16, u16, u16, u16); 9] = [
+    (0x048d, 0xc995, 0xff89, 0x00cc), // 2024 // Legion pro 5i - maybe all 2024?
     (0x048d, 0xc985, 0xff89, 0x00cc), // 2023
     (0x048d, 0xc984, 0xff89, 0x00cc), // 2023
     (0x048d, 0xc983, 0xff89, 0x00cc), // 2023 LOQ
