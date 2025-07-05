f02bd95f42a9aec946f9c95bc1872dca65180a0c
4JX <79868816+4JX@users.noreply.github.com>
2025-07-05 23:12:38 +0200

    Test add for #214

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -11,7 +11,8 @@ use std::{
 
 pub mod error;
 
-const KNOWN_DEVICE_INFOS: [(u16, u16, u16, u16); 11] = [
+const KNOWN_DEVICE_INFOS: [(u16, u16, u16, u16); 12] = [
+    (0x048d, 0xc195, 0xff89, 0x00cc), // 2025 Pro
     (0x048d, 0xc995, 0xff89, 0x00cc), // 2024 Pro
     (0x048d, 0xc994, 0xff89, 0x00cc), // 2024
     (0x048d, 0xc993, 0xff89, 0x00cc), // 2024 LOQ
